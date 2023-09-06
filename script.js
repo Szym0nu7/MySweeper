2949b2fc5aaa003a6f9b1461c7c0bbd70a9a35e8
Szym0nu7 <91138890+Szym0nu7@users.noreply.github.com>
2023-09-06 10:20:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let mines
+let mines;
+let flags;
 const mapHeight = 10;
 const mapWidth = 10;
-const mineamount = 40;
+const mineamount = 10;
 
 function generate() {
-  mines = Array(mapHeight).fill().map(() => Array(mapWidth).fill(0));
+  mines = Array(mapHeight)
+    .fill()
+    .map(() => Array(mapWidth).fill(0));
+  flags = Array(mapHeight)
+    .fill()
+    .map(() => Array(mapWidth).fill(0));
 
-  tableMaker(mapWidth, mapHeight)
+  tableMaker(mapWidth, mapHeight);
 
   for (var y = 0; y < mapHeight; y++) {
     for (var x = 0; x < mapWidth; x++) {
@@ -15,8 +21,6 @@ function generate() {
       }
     }
   }
-  
-  typing();
 }
 
 function tableMaker(tableWidth, tableHeight) {
@@ -24,16 +28,58 @@ function tableMaker(tableWidth, tableHeight) {
   for (y = 0; y < tableHeight; y++) {
     output += "<tr>";
     for (x = 0; x < tableWidth; x++) {
-      //output += "<td>1</td>";
       output +=
-        "<td class='cell' id='" + y.toString() + x.toString() + "'></td>";
+        "<td class='cell' id='" +
+        y.toString() +
+        x.toString() +
+        "' style='background:lightgrey' onclick='cellUncover(" +
+        y +
+        "," +
+        x +
+        ")' oncontextmenu='flagToggle(" +
+        y +
+        "," +
+        x +
+        ")'></td>";
     }
     output += "</tr>";
   }
   putmine();
-  document.getElementById("fu").innerHTML =
-    "<table>" + output + "</table>";
+  document.getElementById("fu").innerHTML = "<table>" + output + "</table>";
   console.table(mines);
+}
+
+function cellUncover(y, x) {
+  console.log(mines[y][x]);
+  if (flags[y][x] === 1) return;
+  document.getElementById(y.toString() + x.toString()).style.background =
+    "white";
+  field = mines[y][x];
+  if (field == 0) {
+    document.getElementById(y.toString() + x.toString()).innerHTML = " ";
+    uncoverEmptyTouchingTiles(y, x);
+  } else if (field == -1) {
+    showMines();
+  } else document.getElementById(y.toString() + x.toString()).innerHTML = field;
+}
+
+function flagToggle(y, x) {
+  if (
+    document.getElementById(y.toString() + x.toString()).style.background !=
+    "white"
+  ) {
+    if (flags[y][x] === 0) {
+      document.getElementById(y.toString() + x.toString()).innerHTML =
+        "<img src='flag.jpg' alt='flag' height='20px' />";
+      flags[y][x] = 1;
+    } else {
+      document.getElementById(y.toString() + x.toString()).innerHTML = "";
+      flags[y][x] = 0;
+    }
+
+    // flags[y][x] = flags[y][x] === 0 ? 1 : 0;
+    console.log(flags[y][x]);
+  }
 }
 
 function putmine() {
@@ -44,6 +90,18 @@ function putmine() {
   }
 }
 
+function showMines() {
+  for (let y = 0; y < mapHeight; y++) {
+    for (let x = 0; x < mapWidth; x++) {
+      if (mines[y][x] == -1) {
+        document.getElementById(y.toString() + x.toString()).style =
+          "background:white";
+        document.getElementById(y.toString() + x.toString()).innerHTML =
+          "<img src='mine.png' width='20px'>";
+      }
+    }
+  }
+}
 // function run() {
 //   for (var y = 0; y < border; y++) {
 //     for (var x = 0; x < xborder; x++) {
@@ -56,62 +114,95 @@ function putmine() {
 // }
 
 function writing(xPos, yPos) {
-  console.log(yPos + " " + xPos);
-
   if (yPos != mapHeight - 1 && xPos != 0)
     if (mines[yPos + 1][xPos - 1] > -1) mines[yPos + 1][xPos - 1] += 1;
 
-  if (yPos != mapHeight - 1) 
+  if (yPos != mapHeight - 1)
     if (mines[yPos + 1][xPos] > -1) mines[yPos + 1][xPos] += 1;
 
   if (yPos != mapHeight - 1 && xPos != mapHeight - 1)
     if (mines[yPos + 1][xPos + 1] > -1) mines[yPos + 1][xPos + 1] += 1;
 
-  if (xPos != 0) 
-    if (mines[yPos][xPos - 1] > -1) mines[yPos][xPos - 1] += 1;
+  if (xPos != 0) if (mines[yPos][xPos - 1] > -1) mines[yPos][xPos - 1] += 1;
 
-  if (xPos != mapHeight - 1) 
+  if (xPos != mapHeight - 1)
     if (mines[yPos][xPos + 1] > -1) mines[yPos][xPos + 1] += 1;
 
-  if (yPos != 0 && xPos != 0) 
+  if (yPos != 0 && xPos != 0)
     if (mines[yPos - 1][xPos - 1] > -1) mines[yPos - 1][xPos - 1] += 1;
 
-  if (yPos != 0) 
-    if (mines[yPos - 1][xPos] > -1) mines[yPos - 1][xPos] += 1;
+  if (yPos != 0) if (mines[yPos - 1][xPos] > -1) mines[yPos - 1][xPos] += 1;
 
   if (yPos != 0 && xPos != mapHeight - 1)
     if (mines[yPos - 1][xPos + 1] > -1) mines[yPos - 1][xPos + 1] += 1;
 }
 
-function typing() {
-  for (var y = 0; y < mapHeight; y++) {
-    for (var x = 0; x < mapWidth; x++) {
-      var field = mines[y][x];
-      if (field == 0)
-        document.getElementById(y.toString() + x.toString()).innerHTML = " ";
-      else if (field == -1)
-        document.getElementById(y.toString() + x.toString()).innerHTML =
-          "<img src='mine.png' width='20px'>";
-      else
-        document.getElementById(y.toString() + x.toString()).innerHTML = field;
-    }
-  }
+function uncoverEmptyTouchingTiles(yPos, xPos) {
+  // console.log(document.getElementById(parseInt(yPos-1) +''+ xPos).style)
+  // top
+  if (
+    yPos != 0 &&
+    document.getElementById(parseInt(yPos - 1) + "" + xPos).style.background !=
+      "white"
+  )
+    cellUncover(yPos - 1, xPos);
+  // right
+  if (
+    xPos != mapWidth - 1 &&
+    document.getElementById(yPos + "" + parseInt(xPos + 1)).style.background !=
+      "white"
+  )
+    cellUncover(yPos, xPos + 1);
+  // bottom
+  if (
+    yPos != mapHeight - 1 &&
+    document.getElementById(parseInt(yPos + 1) + "" + xPos).style.background !=
+      "white"
+  )
+    cellUncover(yPos + 1, xPos);
+  // left
+  if (
+    xPos != 0 &&
+    document.getElementById(yPos + "" + parseInt(xPos - 1)).style.background !=
+      "white"
+  )
+    cellUncover(yPos, xPos - 1);
+
+  // top right
+  if (
+    yPos != 0 &&
+    xPos != mapWidth - 1 &&
+    document.getElementById(parseInt(yPos - 1) + "" + parseInt(xPos + 1)).style
+      .background != "white" &&
+    mines[yPos - 1][xPos + 1] > 0
+  )
+    cellUncover(yPos - 1, xPos + 1);
+
+  // bottom right
+  if (
+    yPos != mapHeight - 1 &&
+    xPos != mapWidth - 1 &&
+    document.getElementById(parseInt(yPos + 1) + "" + parseInt(xPos + 1)).style
+      .background != "white" &&
+    mines[yPos + 1][xPos + 1] > 0
+  )
+    cellUncover(yPos + 1, xPos + 1);
+  // bottom left
+  if (
+    yPos != mapHeight - 1 &&
+    xPos != 0 &&
+    document.getElementById(parseInt(yPos + 1) + "" + parseInt(xPos - 1)).style
+      .background != "white" &&
+    mines[yPos + 1][xPos - 1] > 0
+  )
+    cellUncover(yPos + 1, xPos - 1);
+  // top left
+  if (
+    yPos != 0 &&
+    xPos != 0 &&
+    document.getElementById(parseInt(yPos - 1) + "" + parseInt(xPos - 1)).style
+      .background != "white" &&
+    mines[yPos - 1][xPos - 1] > 0
+  )
+    cellUncover(yPos - 1, xPos - 1);
 }
-
-//var checkingList = [
-//     mines[y - 1][x - 1],
-//     mines[y - 1][x],
-//     mines[y - 1][x + 1],
-//     mines[y][x - 1],
-//     mines[y][x + 1],
-//     mines[y + 1][x - 1],
-//     mines[y + 1][x],
-//     mines[y + 1][x + 1],
-//   ];
-
-//   for (let ifs = 0; ifs <= 8; ifs++) {
-//     if (checkingList[ifs].x == null && checkingList[ifs].y == null)
-//       console.log("Null space");
-//     else {
-//       checkingList[ifs] += 1;
-//     }
